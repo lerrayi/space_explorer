1f1b74ca29d52db8bc35fb1b94021c5606ccd8de
Betty Ferraro <bferraro@gmu.edu>
2025-07-03 22:47:56 +0000

    Add fetch

--- js/script.js
+++ js/script.js
@@ -7,3 +7,15 @@ const endInput = document.getElementById('endDate');
 // - Default to a range of 9 days (from 9 days ago to today)
 // - Restrict dates to NASA's image archive (starting from 1995)
 setupDateInputs(startInput, endInput);
+
+async function fetchImageOfTheDay(date) {
+    const apiKey = "lI2KOgbdKZatYeUAfiPx95ajy7cMXg2kuR8gST4v";
+    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${date}`);
+    if (!response.ok) {
+        throw new Error("Failed to fetch image of the day");
+    }
+    return await response.json();
+}
+
+async function displayImageOfTheDay() {
+}
